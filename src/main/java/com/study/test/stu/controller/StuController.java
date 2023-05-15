fcbdb5ad788c224d0d01c211ea58db0c61cad92e
ILDOO0609 <no.7chelsea@hanmail.net>
2023-05-15 16:37:20 +0900

    2023-05-15 16:37 ILDOO

---
+++ src/main/java/com/study/test/stu/controller/StuController.java
@@ -0,0 +1,10 @@
+package com.study.test.stu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/stu")
+public class StuController {
+
+}

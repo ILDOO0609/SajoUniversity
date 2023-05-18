248718cabe2453acbe01ea0776822dc8877987c3
myeongseok5862 <audtjr5862@gmail.com>
2023-05-18 17:32:58 +0900

    ms2차

---
+++ src/main/java/com/study/test/util/ConstVariable.java
@@ -0,0 +1,6 @@
+package com.study.test.util;
+
+public class ConstVariable {
+	//첨부파일 경로
+	public static final String UPLOAD_PATH = "";
+}
--- src/main/resources/static/js/content/emp/reg_lecture.js
+++ src/main/resources/static/js/content/emp/reg_lecture.js
@@ -1,18 +1,23 @@
-function checkTime(){
-	//ajax start
-	$.ajax({
-		url: '/emp/timeCheckAjax', //요청경로
-		type: 'post',
-		async: true,
-		contentType : 'application/json; charset=UTF-8',
-		contentType : 'application/x-www-form-urlencoded; charset=UTF-8',
-		data: {}, //필요한 데이터
-		success: function(result) {
-			alert('ajax 통신 성공');
-		},
-		error: function() {
-			alert('실패');
-		}
-	});
-//ajax end
+//전공대학 변경시 실행되는 함수
+function changeColl(coll){
+	const collNo = coll.value;
+	
+		//ajax start
+		$.ajax({
+			url: '/emp/changeCollAjax', //요청경로
+			type: 'post',
+			async: true,
+			contentType : 'application/x-www-form-urlencoded; charset=UTF-8',
+			data: {'collNo':collNo}, //필요한 데이터
+			success: function(result) {
+				alert('ajax 통신 성공');
+				console.log(result);
+				alert(result);
+			},
+			error: function() {
+				alert('실패');
+			}
+		});
+	//ajax end
 }
+

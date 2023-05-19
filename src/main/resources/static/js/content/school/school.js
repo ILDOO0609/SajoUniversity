a493e3d4f451a17e7772e31942ddc4ecc06ed9bf
nkds5009 <nkds5009@gmail.com>
2023-05-19 15:50:03 +0900

    0519 15:40

--- src/main/resources/static/js/content/school/school.js
+++ src/main/resources/static/js/content/school/school.js
@@ -1,4 +1,4 @@
-//select 박스(년도) 값 변경시 실행
+//학사인포 년도 셀렉트 변경시 실행
 function getYearSelect(){
 	const year = document.querySelector('#yearSelect').value;
 	location.href = `/school/info?year=${year}`;
@@ -8,13 +8,51 @@ function getYearSelect(){
 //검색버튼 클릭시 실행
 function infoSearchList(){
 	const search_form = document.querySelector('#searchForm');
-	const checkd_cnt = search_form.querySelectorAll('.form-select').length;
 	
-	if(checkd_cnt == 0){
-		alert('검색할 주문상태를 선택하세요');
-		return;
-	}
-
+	
 	search_form.submit();	
 	
+}
+
+
+//--------------학사조회-----------------------------------------------
+
+
+//전공대학 변경시 실행되는 함수
+function changeColl(coll){
+	const coll_no = coll.value;
+	
+		//ajax start
+		$.ajax({
+			url: '/emp/changeCollAjax', //요청경로
+			type: 'post',
+			async: true,
+			contentType : 'application/x-www-form-urlencoded; charset=UTF-8',
+			data: {'collNo':coll_no}, //필요한 데이터
+			success: function(result) {
+				console.log(result);
+				drawDeptSelectbox(result);
+			},
+			error: function() {
+				alert('실패');
+			}
+		});
+	//ajax end
+}
+
+//전공학과 셀렉트박스 그리기
+function drawDeptSelectbox(deptList){
+	const dept_div = document.querySelector('#deptDiv');
+	
+	dept_div.replaceChildren();
+		
+	let str = '';
+	
+	str += `<select class="form-select" name="deptNo">`;
+	for(const dept of deptList){
+		str += `<option value="${dept.deptNo}">${dept.deptName}</option>`;
+	}
+	str += `</select>`;
+	
+	dept_div.insertAdjacentHTML('afterbegin',str);
 }

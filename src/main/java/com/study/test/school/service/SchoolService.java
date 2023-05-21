2cfc0694cfab02528b6ee3aa610462bb78213763
nkds5009 <nkds5009@gmail.com>
2023-05-22 00:08:34 +0900

    05-22 00:08

--- src/main/java/com/study/test/school/service/SchoolService.java
+++ src/main/java/com/study/test/school/service/SchoolService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import com.study.test.colleage.vo.DeptVO;
 import com.study.test.school.vo.PrenextPageVO;
+import com.study.test.school.vo.SchSearchVO;
 import com.study.test.school.vo.SchoolInfoVO;
+import com.study.test.util.PageVO;
 
 public interface SchoolService {
 
@@ -12,7 +14,7 @@ public interface SchoolService {
 	void insertSchoolInfo(SchoolInfoVO schoolInfoVO);
 
 	//학사안내 게시글 목록
-	List<SchoolInfoVO> getSchoolInfoList();
+	List<SchoolInfoVO> getSchoolInfoList(PageVO pageVO);
 	
 	//학사안내 게시글 -> 상세조회 
 	SchoolInfoVO schoolBoardDetail(String schInfoCode);
@@ -21,7 +23,7 @@ public interface SchoolService {
 	void updateSchoolBoardReadCnt(String schInfoCode);
 	
 	//학사안내 게시글 이전글 다음글
-	List<PrenextPageVO> detailPreNext();
+	SchoolInfoVO movePage(String schInfoCode);
 	
 	//학사안내 게시글 -> 상세 -> 수정페이지 이동
 	String updateSchoolInfoForm(String schInfoCode);
@@ -32,6 +34,12 @@ public interface SchoolService {
 	//학사안내 게시글 -> 상세 -> 삭제
 	void deleteSchoolInfo(String schInfoCode); 
 	
+	//학사안내 게시글 전체 데이터 수 조회
+	int schInfoListCnt();
+	
+	
+	
+
 	
 // -------학사 조회------------------------------------------------------	
 	//강의등록시 전공대학 선택시 해당하는 전공학과 이름 조회

c2463c1d4cd0284d65e98ece868bf767ff25cd41
myeongseok5862 <audtjr5862@gmail.com>
2023-06-08 15:46:45 +0900

    d

--- src/main/java/com/study/test/school/service/SchoolServiceImpl.java
+++ src/main/java/com/study/test/school/service/SchoolServiceImpl.java
@@ -393,6 +393,11 @@ public class SchoolServiceImpl implements SchoolService{
 		return sqlSession.selectOne("schoolMapper.getMemberDeniedListCnt");
 	}
 
+	@Override
+	public int getStatusCntForI() {
+		return 0;
+	}
+
 	
 
 
